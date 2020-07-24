eca921493b8c6bd9f08089807b4a5ea8b5278911
黄辰杰 <huangchenjie@huangchenjiedeMacBook-Pro.local>
2020-07-24 15:22:47 +0800

    init

---
+++ src/main/java/com/yepu/teamcity/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.yepu.teamcity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}
